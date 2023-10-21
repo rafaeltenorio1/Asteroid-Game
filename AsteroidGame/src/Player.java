b2138a8257b64ecbe8075d3f9d25def3f409d96c
Rafael Tenorio <132300643+rafaeltenorio1@users.noreply.github.com>
2023-10-21 02:56:50 -0300

    Delete AsteroidGame/src/Player.java

--- AsteroidGame/src/Player.java
+++
@@ -1,8 +0,0 @@
-import java.awt.image.BufferedImage;
-
-public class Player extends Objetos {
-
-    public Player(double x, double y, double width, double height, BufferedImage sprite) {
-        super(x, y, width, height, sprite);
-    }
-}
